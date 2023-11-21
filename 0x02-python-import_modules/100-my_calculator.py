798e6bdc1307b2c8988b30575e08752db1e252a7
Pascal7601 <levispascal63@gmail.com>
2023-11-21 02:21:36 -0800

    module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 import sys
-from realcalc import add, div, mul, sub
+from calculator_1 import add, div, mul, sub
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 sum = 0
 for i in sys.argv[1:]:
